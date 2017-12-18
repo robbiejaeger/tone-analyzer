5d4f8e9181679f69b91d82dac76429b0709a301d
Robbie Jaeger <jaeger.rob@gmail.com>
2017-12-17 17:27:05 -0700

    Add sentences optoin to tone analysis request

--- analyzer.js
+++ analyzer.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 
+const tripReportData = require('./data/trip-reports.json');
+
+const infoTextBlock = tripReportData.map((report) => {
+  return report.info;
+}).join(' ');
+
 const ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
 
 const tone_analyzer = new ToneAnalyzerV3({
@@ -9,8 +15,9 @@ const tone_analyzer = new ToneAnalyzerV3({
 });
 
 const params = {
-  tone_input: 'Greetings from Watson Developer Cloud!',
-  content_type: 'text/plain'
+  tone_input: infoTextBlock,
+  content_type: 'text/plain',
+  sentences: false  
 };
 
 tone_analyzer.tone(params, (err, tone) => {
